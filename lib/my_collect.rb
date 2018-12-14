105ffb1292e6fa7a8d10b511599435581fb2c2a7
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-14 18:36:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
-
-
+def my_collect(collection)
+  counter = 0
+  output = []
+  
+  while counter < collection.length
+    if collection[counter].split.length == 1
+      yield collection[counter].upcase
+      output.push(collection[counter].upcase)
+      counter += 1
+    else
+      yield collection[counter].split(" ").first
+      output.push(collection[counter].split(" ").first)
+      counter += 1
+    end
+  end
+  
+  output
+end
