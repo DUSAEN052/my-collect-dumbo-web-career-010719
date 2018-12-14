27924092fbe7216758c07189e5850d30812a5b8f
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-14 18:29:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(collection)
+  counter = 0
+  output = []
+  while counter < collection.length
+    if collection[counter].split.length == 1
+      yield collection[counter].upcase
+      output.push(collection[counter].upcase)
+      count += 1
+    else
+      yield collection[counter].split[0]
+      output.push(collection[counter].split[0])
+      count += 1
+    end
+  end
+  output
+end
